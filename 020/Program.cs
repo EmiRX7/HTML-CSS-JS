de11fbea8ea8c01671c8b76aac7845903689bdf8
FoxRX7 <emil0903@yandex.ru>
2023-02-06 21:29:53 +0300

    Lesson c#

---
+++ 020/Program.cs
@@ -0,0 +1,13 @@
+﻿// 20 Дано число. Проверить кратно ли оно 7 и 23
+
+int a, b, c;
+Console.Write("Введите число: ");
+do
+{
+  a = Convert.ToInt32(System.Console.ReadLine());
+  b = a%23;
+  c = a%7;
+}
+while (b > 1 | c > 1);
+
+Console.WriteLine($"{a} кратно 7 и 23");
---
+++ 022/Program.cs
@@ -0,0 +1,14 @@
+﻿// 22 По двум заданным числам проверять является ли одно квадратом другого.
+
+double a, b, c=0;
+
+do
+{
+    Console.Write("Введите число a: ");
+    a = Convert.ToInt32(System.Console.ReadLine());
+    Console.Write("Введите число b: ");
+    b = Convert.ToInt32(System.Console.ReadLine());
+    c = Math.Pow(a, 2);
+}
+while (c!=b);
+Console.Write($"{c} является квадратом числа {a}");
