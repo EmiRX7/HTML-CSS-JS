379021661cb4a3c5b71677363f03c74b32d62806
FoxRX7 <emil0903@yandex.ru>
2023-02-14 15:09:41 +0300

    Lesson c#

---
+++ 051/Program.cs
@@ -0,0 +1,27 @@
+﻿// 51 С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+
+int Math()
+{
+    int counter = 0;
+    System.Console.WriteLine("Введите количество чисел");
+    int N = Convert.ToInt32(System.Console.ReadLine());
+    int[] Massiv = new int[N];
+    System.Console.WriteLine("Введите числа");
+    for(int i=0; i<N; i++)
+    {
+        int a = Convert.ToInt32(System.Console.ReadLine());
+        Massiv[i]=a;
+    }
+    for(int i=0; i<N; i++)
+    {
+        int a = Massiv[i];
+        if (a>0)
+        {
+            counter++;
+        }
+    }
+    System.Console.WriteLine(counter);
+    return 0;
+}
+
+System.Console.WriteLine(Math());
---
+++ 056/Program.cs
@@ -0,0 +1,27 @@
+﻿// 56 Написать программу копирования массива
+
+int Math()
+{
+    System.Console.WriteLine("Введите количество чисел");
+    int N = Convert.ToInt32(System.Console.ReadLine());
+    int[] Massiv = new int[N];
+    System.Console.WriteLine("Введите числа");
+    for(int i=0; i<N; i++)
+    {
+        int a = Convert.ToInt32(System.Console.ReadLine());
+        Massiv[i]=a;
+    }
+    int[] MassivCop = new int[N];
+    for(int i=0; i<N; i++)
+    {
+        int a = Massiv[i];
+        MassivCop[i] = a;
+    }
+    foreach (int i in MassivCop)
+    {
+        System.Console.Write($"{i} ");
+    }
+    return 0;
+}
+
+System.Console.WriteLine(Math());
