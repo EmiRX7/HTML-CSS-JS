ea70fe932a779263c22d0f2f5b178f0f2de790b1
FoxRX7 <emil0903@yandex.ru>
2023-03-13 19:02:42 +0300

    C#

---
+++ 084/Program.cs
@@ -0,0 +1,14 @@
+﻿// 84 Проверить на число и найти сумму цифр числа
+
+string chislo = Console.ReadLine();
+int sum=0;
+for(int i = 0; i < chislo.Length; i++)
+{
+    if(char.IsDigit(chislo[i]))
+    {
+        sum += chislo[i] - 48;
+        Console.WriteLine(sum);
+    }
+    else 
+    Console.WriteLine("Ne chislo");
+}
