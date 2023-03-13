94b6cb19e836cdfb063349dc4735b0c81d60529f
FoxRX7 <emil0903@yandex.ru>
2023-03-13 19:01:39 +0300

    C#

---
+++ 086/Program.cs
@@ -0,0 +1,13 @@
+﻿// 86 Подсчитать сколько раз определенный символ (например 'a') встречается в строке.
+
+Console.WriteLine("Напишите текст");
+string text = Console.ReadLine();
+int kol=0;
+for(int i = 0; i < text.Length; i++)
+{
+    if(text[i] == 'a')
+    {
+        kol++;
+    }
+}
+System.Console.WriteLine("Буква a встречается " + kol + " раз" );
