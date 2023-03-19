d9634688e63593bed504a0f750fadfb95d9adfd4
FoxRX7 <emil0903@yandex.ru>
2023-03-19 19:12:51 +0300

    C# кр Ввод и Вывод

---
+++ C#KR/Program.cs
@@ -0,0 +1,21 @@
+﻿// 
+Console.WriteLine("Введите 4 строки");
+string[] stroki = new string[4];
+string[] strokiSort = new string[4];
+for(int i=0; i<4; i++)
+{
+    string n;
+    n = Console.ReadLine();
+    stroki[i]=n;
+    int m=0;
+    for(int j=0; j<n.Length; j++)
+        m++;
+    if(m<4)
+    {
+        strokiSort[i]=n;
+    }
+}
+for(int i=0; i<stroki.Length; i++)
+    Console.Write($"{stroki[i]} ");
+for(int i=0; i<stroki.Length; i++) 
+    Console.Write($"{strokiSort[i]} ");
